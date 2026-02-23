de278f48bf053ef1890444b0e8e614e29f592894
Linss-007 <cristopheradl7@gmail.com>
2026-02-22 22:24:12 -0600

    Definición de variables e ingreso de datos

--- 3- Facturacion/Program.cs
+++ 3- Facturacion/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿int tipoCliente;
+double montoBase;
+int metodoPago;
+string cupon;
+string codigo;
+int antifraude;
+Console.WriteLine("Sistema de facturación");
+Console.WriteLine("Ingrese el tipo de cliente");
+Console.WriteLine("1. Estudiante");
+Console.WriteLine("2. Docente");
+Console.WriteLine("3. Administrativo");
+Console.WriteLine("4. Externo");
+tipoCliente = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el monto base a pagar");
+montoBase = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el metodo de pago");
+Console.WriteLine("1. Efectivo");
+Console.WriteLine("2. Tarjeta");
+Console.WriteLine("3. Transferencia");
+metodoPago  = int.Parse(Console.ReadLine());
+Console.WriteLine("¿Posee cupon? S/N");
+cupon = Console.ReadLine();
+Console.WriteLine("Reporte antifraude");
+Console.WriteLine("1. Ninguno");
+Console.WriteLine("2. Cupon invalido o repetido");
+Console.WriteLine("3. Pagos rechazados multiples");
+antifraude = int.Parse(Console.ReadLine());
