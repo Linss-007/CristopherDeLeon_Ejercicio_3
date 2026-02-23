b46b7f35899f05a9be4e1f3b7082f243679ecb4f
Linss-007 <cristopheradl7@gmail.com>
2026-02-22 23:04:20 -0600

    Switch restantes y descuentos segun cliente

--- 3- Facturacion/Program.cs
+++ 3- Facturacion/Program.cs
@@ -4,7 +4,8 @@ int metodoPago;
 string cupon;
 string codigo;
 int antifraude;
-double descuento;
+double descuento = 1;
+double recargo = 1;
 Console.WriteLine("Sistema de facturación");
 Console.WriteLine("Ingrese el tipo de cliente");
 Console.WriteLine("1. Estudiante");
@@ -40,6 +41,10 @@ switch (metodoPago)
     case 3:
         descuento = 1;
         break;
+
+    default:
+        Console.WriteLine("Metodo de pago no válido");
+        break;
 }
 
 if (cupon == "S" || cupon == "s")
@@ -54,4 +59,59 @@ if (cupon == "S" || cupon == "s")
     {
         descuento = 1.15;
     }
+}
+
+switch(antifraude)
+{
+    case 1:
+
+    break;
+
+    case 2:
+        descuento = 1;
+        recargo = 1.10;
+        Console.WriteLine("Se le asigno recargo\n");
+    break;
+
+    case 3:
+        descuento = 1;
+        recargo = 1.10;
+        Console.WriteLine("Se le asigno recargo\n");
+    break;
+
+    default:
+        Console.WriteLine("Antifraude no válido");
+    break;
+}
+
+switch(tipoCliente)
+{
+    case 1:
+        Console.WriteLine($"Su pago por ser estudiante es de: {(montoBase / descuento) * recargo}");
+        break;
+
+    case 2:
+        Console.WriteLine($"Su pago por ser docente es de: {montoBase * recargo}");
+        break;
+
+    case 3:
+        if(recargo == 1)
+        {
+            descuento = 1.15;
+        }
+        Console.WriteLine($"Su pago por ser administrativo es de: {(montoBase / descuento) * recargo}");
+    break;
+
+    case 4:
+        if (cupon == "s" || cupon == "S")
+        {
+            descuento = 1.15;
+            Console.WriteLine($"Precio por canje de codigo: {montoBase/descuento}");
+        }
+        Console.WriteLine($"Por ser externo su pago es normal: {montoBase}");
+    break;
+
+    default:
+        Console.WriteLine("No existe ese tipo de cliente");
+    break;
 }
