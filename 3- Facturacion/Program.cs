8a79b3592deb98de46502f615481840ef34b6dda
Linss-007 <cristopheradl7@gmail.com>
2026-02-22 22:43:25 -0600

    Primer switch y condicional

--- 3- Facturacion/Program.cs
+++ 3- Facturacion/Program.cs
@@ -4,6 +4,7 @@ int metodoPago;
 string cupon;
 string codigo;
 int antifraude;
+double descuento;
 Console.WriteLine("Sistema de facturación");
 Console.WriteLine("Ingrese el tipo de cliente");
 Console.WriteLine("1. Estudiante");
@@ -18,10 +19,39 @@ Console.WriteLine("1. Efectivo");
 Console.WriteLine("2. Tarjeta");
 Console.WriteLine("3. Transferencia");
 metodoPago  = int.Parse(Console.ReadLine());
-Console.WriteLine("¿Posee cupon? S/N");
-cupon = Console.ReadLine();
 Console.WriteLine("Reporte antifraude");
 Console.WriteLine("1. Ninguno");
 Console.WriteLine("2. Cupon invalido o repetido");
 Console.WriteLine("3. Pagos rechazados multiples");
 antifraude = int.Parse(Console.ReadLine());
+Console.WriteLine("¿Posee cupon? S/N");
+cupon = Console.ReadLine();
+
+switch (metodoPago)
+{
+    case 1:
+        descuento = 1.10;
+        break;
+
+    case 2:
+        descuento = 1.07;
+        break;
+
+    case 3:
+        descuento = 1;
+        break;
+}
+
+if (cupon == "S" || cupon == "s")
+{
+    Console.WriteLine("Ingrese el codigo del cupon");
+    codigo = Console.ReadLine();
+    if (codigo.Contains("U") || codigo.Contains("2"))
+    {
+        Console.WriteLine("Codigo no válido");
+    }
+    else
+    {
+        descuento = 1.15;
+    }
+}
